58d6bbc53889dd819479373b6d21d71934233bd9
farinhote <andrefarinhote@gmail.com>
2025-04-15 12:56:33 +0100

    ai suggestion dialog

--- client/src/components/dashboard/AiSuggestionDialog.tsx
+++ client/src/components/dashboard/AiSuggestionDialog.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTheme } from '../../context/ThemeContext';
 import './AiSuggestionDialog.css';
 
 interface AiSuggestionDialogProps {
@@ -18,6 +19,8 @@ const AiSuggestionDialog: React.FC<AiSuggestionDialogProps> = ({
   onReject,
   action
 }) => {
+  const { isDarkMode } = useTheme();
+  
   if (!isVisible) return null;
 
   return (
